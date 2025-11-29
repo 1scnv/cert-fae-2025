ba84808d9aa26e15db8c02f6945e22138cf26a9e
Vinicius Soares <suiciniv-s@hotmail.com>
2025-11-29 05:45:16 +0000

    stg.erp.person casting types

--- models/staging/erp/person/stg__person_countryregion.sql
+++ models/staging/erp/person/stg__person_countryregion.sql
@@ -1,8 +1,8 @@
 with
     source as (
         select
-            countryregioncode
-            , name
+            cast(countryregioncode as int) as countryregioncode
+            , cast(name as string) as name
         from {{source('raw_person_aw','person_countryregion')}}
     )
 
