4843cc2cbabc316dab32500caffad4c8a151b923
Vinicius Soares <suiciniv-s@hotmail.com>
2025-11-29 05:39:35 +0000

    renamed column group

--- models/staging/erp/sales/stg__sales_salesterritory.sql
+++ models/staging/erp/sales/stg__sales_salesterritory.sql
@@ -5,7 +5,7 @@ with
             cast(territoryid as int) as territoryid
             , cast(name as string) as name
             , cast(countryregioncode as string) as countryregioncode
-            , cast(group as string) as "group"
+            , cast(group as string) as group
         from {{ source("raw_sales_aw", "sales_salesterritory") }}
     )
 
