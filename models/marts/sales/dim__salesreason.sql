864bbc70c18ef62cab0d4dfee323872fedf7ba04
Vinicius Soares <suiciniv-s@hotmail.com>
2025-12-04 05:34:21 +0000

    rename ref

--- models/marts/sales/dim__salesreason.sql
+++ models/marts/sales/dim__salesreason.sql
@@ -1,6 +1,6 @@
 with
     source_salesorderreason as (
-        select * from {{ ref('int__salesource_salesorderreason') }}
+        select * from {{ ref('int__salesorderreason') }}
     )
 
 select * from source_salesorderreason
